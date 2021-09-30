b2dac94d4de6142c2121510038180124bc30cfc6
kirkai <kirkai@mail.uc.edu>
2021-09-30 14:25:21 -0400

    initial setup

---
+++ src/main/java/com/soit/soitfaculty/Soitfaculty21Application.java
@@ -0,0 +1,13 @@
+package com.soit.soitfaculty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Soitfaculty21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Soitfaculty21Application.class, args);
+	}
+
+}
